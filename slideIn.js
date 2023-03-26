c318df90a6b7aba0bb97d4723f8557f4f612ffd2
salimzeeshan <zeeshasalim541@gmail.com>
2023-03-26 22:16:39 +0530

    Added Nike, Inc project

--- slideIn.js
+++ slideIn.js
@@ -1,13 +1,10 @@
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add("show")
-        } else {
-            // entry.target.classList.remove("show")
-        }
-    })
-})
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
 
-const hidden = document.querySelectorAll(".hidden")
-hidden.forEach((el)=>observer.observe(el))
-
+const hidden = document.querySelectorAll(".hidden");
+hidden.forEach((el) => observer.observe(el));
